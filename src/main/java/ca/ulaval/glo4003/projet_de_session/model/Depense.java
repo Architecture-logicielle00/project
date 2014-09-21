4ee05dd15f33b3aab5c7bc6f812336ca2866596a
delislejp <delisle_jp@outlook.com>
2014-09-21 17:24:50 -0400

    Depense (avec etat)

--- src/main/java/ca/ulaval/glo4003/projet_de_session/model/Depense.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/model/Depense.java
@@ -1,13 +1,34 @@
 package ca.ulaval.glo4003.projet_de_session.model;
 
 public class Depense {
+	public enum Etat
+	{
+		enAttente,
+		accepter,
+		refuser
+	}
+	
+	public Depense(Employe _employe, float _montant, String _description, SimpleProjet _projet)
+	{
+		employe = _employe;
+		montant = _montant;
+		description = _description;
+		projet = _projet;
+	}
+	
+	public void confirmation(Etat _etat)
+	{
+		etat = _etat;
+	}
+	
+	public void changerMontant(float _montant)
+	{
+		montant = _montant;
+	}
+	
 	float montant;
 	String description;
 	Employe employe;
-	Projet projet;
-	
-	public Depense(Employe _employe, float _montant, String _description, String projet)
-	{
-		
-	}
+	SimpleProjet projet;
+	Etat etat = Etat.enAttente;
 }
