3402897b0124f723072d283a1dc4d03b75a8b6ab
jfgra21 <jean-francois.gravel.5@ulaval.ca>
2014-09-24 13:46:30 -0400

    Hummm

--- src/main/java/ca/ulaval/glo4003/projet_de_session/web/controllers/ControllerPrincipal.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/web/controllers/ControllerPrincipal.java
@@ -4,13 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ca.ulaval.glo4003.projet_de_session.dao.RepositoryUtilisateur;
 import ca.ulaval.glo4003.projet_de_session.imodel.IIdentificateur;
 import ca.ulaval.glo4003.projet_de_session.imodel.IRepositoryUtilisateur;
 import ca.ulaval.glo4003.projet_de_session.mock.FakeIdentificateur;
+import ca.ulaval.glo4003.projet_de_session.model.FeuilleDeTemps;
 
 @Controller
 public class ControllerPrincipal 
@@ -71,5 +74,17 @@ public class ControllerPrincipal
 	@RequestMapping("/FeuilleDeTemps")
 	public String accederFeuilleDeTemps() {
 		return "timeSheet";
+	}
+	
+	@RequestMapping(value="FeuilleDeTemps", method = RequestMethod.POST)
+	public @ResponseBody Boolean sauvegarderFeuilleDeTemps( @RequestBody final  FeuilleDeTemps feuilleDeTemps)
+	{
+		// Valider la feuilleDeTemps / Sa validité
+		
+		// Sauvegarder la feuille de temps
+		
+		boolean sauvegardeEffectueAvecSucces = true;
+		
+		return sauvegardeEffectueAvecSucces;
 	}
 }
