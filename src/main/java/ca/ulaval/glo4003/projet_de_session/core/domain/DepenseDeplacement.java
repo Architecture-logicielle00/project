cf795c1dab8556f164bb3bba30322b53f0a73b9e
LiberT <LiberT@LiberT-PC-12345>
2014-10-10 16:13:58 -0400

    Creation des classes depense, depense deplacement, et depense diverses. Creation des formulaires de depense et deplacement view model le frame.

---
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/core/domain/DepenseDeplacement.java
@@ -0,0 +1,26 @@
+package ca.ulaval.glo4003.projet_de_session.core.domain;
+
+public class DepenseDeplacement extends Depense {
+
+	private float distance;
+
+	public float getDistance() {
+		return distance;
+	}
+
+	public void setDistance(float distance) {
+		this.distance = distance;
+
+	}
+
+	private float coutkm;
+
+	public float getCoutKm() {
+		return coutkm;
+	}
+
+	public void setValue(float coutkm) {
+		this.coutkm = coutkm;
+	}
+
+}
---
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/web/viewmodels/FeuilleDeDepenseViewModel.java
@@ -0,0 +1,20 @@
+package ca.ulaval.glo4003.projet_de_session.web.viewmodels;
+
+import java.util.ArrayList;
+
+public class FeuilleDeDepenseViewModel {
+	// Déclarations de variables
+
+	public ArrayList<FeuilleDeDepenseViewModel> depenses;
+
+	public FeuilleDeDepenseViewModel() {
+		depenses = new ArrayList<FeuilleDeDepenseViewModel>();
+	}
+
+	// Fonction pour le total
+	public ArrayList<String> obtListeTotalDepense() {
+		return null;
+
+	}
+
+}
