cffeef8c6e12f26dbf49682ac064df06ee2abd1f
LiberT <LiberT@LiberT-PC-12345>
2014-10-13 20:13:30 -0400

    ...

--- src/main/webapp/resources/js/deplacementForm/deplacementForm.js
+++ src/main/webapp/resources/js/deplacementForm/deplacementForm.js
@@ -1,70 +1,65 @@
-$(document).ready(function(){
-    ajouterNouvelleLigne();
-    initialiserEvenements();
+$(document).ready(function() {
+	ajouterNouvelleLigne();
+	initialiserEvenements();
 })
 
-function sauvegarder(){
-    alert("test");
-    
-  //TODO 
-    //pas encore complet en essai...
-	   }
-    $.ajax({
-    	  type: "POST",
-    	  url:"/deplacementForm"
-    	  data: JSON.stringify(depense),
-    	  success: success,
-    	  dataType: dataType
-    	});
+function sauvegarder() {
+	alert("test");
+	//TODO
 }
 
-function ajouterNouvelleLigne(){
-    $("#table-depense-deplacement").append(rowTemplate);
+function ajouterNouvelleLigne() {
+	$("#table-depense-deplacement").append(rowTemplate);
 }
 
-function supprimerLignesSelectionne(){
-        $("#table-depense-deplacement").find('tr > td > input:checked').parent().parent().remove();
-        $(".sous-total").change();}
-
-function initialiserEvenements(){
-    $("#ajouter-nouvelle-ligne").on("click", function(){
-        ajouterNouvelleLigne();
-    });
-
-    $("#supprimer-nouvelle-ligne").on("click", function(){
-        supprimerLignesSelectionne();
-    });
-
-    $("#sauvegarder-feuille").on("click", function(){
-            sauvegarder();
-    });
-
-    $('body').on("change", ".row-input", function(){
-        var $row = $(this).parent().parent();
-        var $inputsRow = $row.find(".row-input");
-
-        var sousTotalCalcul = $inputsRow.filter(".distance").val() * $inputsRow.filter(".cout-km").val() +
-                                $inputsRow.filter(".nb-repas").val() * $inputsRow.filter(".cout-repas").val() +
-                                ($inputsRow.filter(".coucher").val() * 1) +
-                                ($inputsRow.filter(".divers").val() * 1 );
-
-        $row.find(".sous-total").val(sousTotalCalcul);
-        $row.find(".sous-total").change();
-    });
-
-    $('body').on("change", ".sous-total", function(){
-            var total = 0;
-            $(".sous-total").each(function(){
-                total += $(this).val() != "" ? parseFloat($(this).val()) : 0 ;
-            })
-
-            $("#total").val(total);
-
-        });
-
-
+function supprimerLignesSelectionne() {
+	$("#table-depense-deplacement").find('tr > td > input:checked').parent()
+			.parent().remove();
+	$(".sous-total").change();
 }
 
+function initialiserEvenements() {
+	$("#ajouter-nouvelle-ligne").on("click", function() {
+		ajouterNouvelleLigne();
+	});
+
+	$("#supprimer-nouvelle-ligne").on("click", function() {
+		supprimerLignesSelectionne();
+	});
+
+	$("#sauvegarder-feuille").on("click", function() {
+		sauvegarder();
+	});
+
+	$('body').on(
+			"change",
+			".row-input",
+			function() {
+				var $row = $(this).parent().parent();
+				var $inputsRow = $row.find(".row-input");
+
+				var sousTotalCalcul = $inputsRow.filter(".distance").val()
+						* $inputsRow.filter(".cout-km").val()
+						+ $inputsRow.filter(".nb-repas").val()
+						* $inputsRow.filter(".cout-repas").val()
+						+ ($inputsRow.filter(".coucher").val() * 1)
+						+ ($inputsRow.filter(".divers").val() * 1);
+
+				$row.find(".sous-total").val(sousTotalCalcul);
+				$row.find(".sous-total").change();
+			});
+
+	$('body').on("change", ".sous-total", function() {
+		var total = 0;
+		$(".sous-total").each(function() {
+			total += $(this).val() != "" ? parseFloat($(this).val()) : 0;
+		})
+
+		$("#total").val(total);
+
+	});
+
+}
 
 var rowTemplate = '<tr> \
                          <td><input type="checkbox"/></td> \
@@ -77,4 +72,3 @@ var rowTemplate = '<tr> \
                          <td><input class="divers row-input" type="text" /></td> \
                          <td><input class="sous-total" type="text" readonly="true"/></td> \
                      </tr>';
-
