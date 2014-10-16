63254933dc8f9c26565089822acf81e2f2117f3f
David <Enusius@users.noreply.github.com>
2014-10-16 11:54:36 -0400

    Create RepoFeuilleDeTempsXmlTest.java

---
+++ src/test/java/ca/ulaval/glo4003/projet_de_session/persistence/repositoryXml/RepoFeuilleDeTempsXmlTest.java
@@ -0,0 +1,60 @@
+package ca.ulaval.glo4003.projet_de_session.persistence.repositoryXml;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RepoFeuilleDeTempsXmlTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//
+	}
+
+	@Test
+	public void testRepoFeuilleDeTempsXml() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAjouter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtTout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupprimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifier() {
+		fail("Not yet implemented");
+	}
+
+}
