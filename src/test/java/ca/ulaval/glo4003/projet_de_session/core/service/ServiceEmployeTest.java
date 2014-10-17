6ebe3ea13079676c39cf6a4734852651ffd33808
David <david.sauve.1@ulaval.ca>
2014-10-17 13:08:23 -0400

    Tests reussi pour RepoEmployeXml

---
+++ src/test/java/ca/ulaval/glo4003/projet_de_session/core/service/ServiceEmployeTest.java
@@ -0,0 +1,79 @@
+package ca.ulaval.glo4003.projet_de_session.core.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ca.ulaval.glo4003.projet_de_session.core.services.ServiceEmploye;
+
+public class ServiceEmployeTest {
+
+	private static ServiceEmploye serviceEmploye;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		serviceEmploye = new ServiceEmploye();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testObtEmploye() {
+		
+	}
+
+	@Test
+	public void testCreerEmploye() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifierEmploye() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtEmployes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtEmployesParEntreprise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifierMotDePasse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtEmployeViewModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtEmployesViewModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDefEmploye() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/ca/ulaval/glo4003/projet_de_session/persistence/repositoryXml/RepoEmployeXmlTest.java
@@ -0,0 +1,138 @@
+package ca.ulaval.glo4003.projet_de_session.persistence.repositoryXml;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ca.ulaval.glo4003.projet_de_session.core.domain.Employe;
+import ca.ulaval.glo4003.projet_de_session.core.domain.FeuilleDeTemps;
+import ca.ulaval.glo4003.projet_de_session.persistence.utils.Xml;
+
+public class RepoEmployeXmlTest {
+
+	private static RepoEmployeXml repoEmployeXml;
+	
+	private static Employe employe1; 
+	private static Employe employe2; 
+	
+	private static Xml<Employe> xmlEmploye;
+	private static ArrayList<Employe> sauvegardeCollection;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		xmlEmploye = new Xml<Employe>(Employe.class);
+		
+		sauvegardeCollection = (ArrayList<Employe>) xmlEmploye.charger("xmlfiles/employes");
+		
+		repoEmployeXml = new RepoEmployeXml();
+		
+		
+		employe1 = Mockito.mock(Employe.class);
+		Mockito.when(employe1.obtNomUtilisateur()).thenReturn("nomUtilisateurTest1");
+		Mockito.when(employe1.obtCodePostal()).thenReturn("codePostalTest1");
+		Mockito.when(employe1.obtEmail()).thenReturn("emailTest1");
+		Mockito.when(employe1.obtEntreprise()).thenReturn("entrepriseTest1");
+		Mockito.when(employe1.obtGenre()).thenReturn("genreTest1");
+		Mockito.when(employe1.obtNom()).thenReturn("nomTest1");
+		
+		employe2 = Mockito.mock(Employe.class);
+		Mockito.when(employe2.obtNomUtilisateur()).thenReturn("nomUtilisateurTest2");
+		Mockito.when(employe2.obtCodePostal()).thenReturn("codePostalTest2");
+		Mockito.when(employe2.obtEmail()).thenReturn("emailTest2");
+		Mockito.when(employe2.obtEntreprise()).thenReturn("entrepriseTest2");
+		Mockito.when(employe2.obtGenre()).thenReturn("genreTest2");
+		Mockito.when(employe2.obtNom()).thenReturn("nomTest2");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		xmlEmploye.enregistrer(sauvegardeCollection, "xmlfiles/employes");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		xmlEmploye.enregistrer(new ArrayList<Employe>(), "xmlfiles/employes");
+		repoEmployeXml = new RepoEmployeXml();
+		
+		assertEquals(new ArrayList<Employe>(), repoEmployeXml.obtEmployes());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAjouter() {
+		repoEmployeXml.ajouter(employe1);
+		
+		assertEquals(employe1, repoEmployeXml.obtenir("nomUtilisateurTest1"));
+	}
+
+	@Test
+	public void testObtenir() {
+		repoEmployeXml.ajouter(employe1);
+		
+		assertEquals(employe1, repoEmployeXml.obtenir("nomUtilisateurTest1"));
+	}
+
+	@Test
+	public void testObtEmployes() {
+		repoEmployeXml.ajouter(employe1);
+		repoEmployeXml.ajouter(employe2);
+		
+		ArrayList<Employe> employes= new ArrayList<Employe>();
+		employes.add(employe1);
+		employes.add(employe2);
+		
+		assertEquals(employes, repoEmployeXml.obtEmployes());
+	}
+
+	@Test
+	public void testSupprimer() {
+		repoEmployeXml.ajouter(employe1);
+		repoEmployeXml.ajouter(employe2);
+		
+		ArrayList<Employe> employes1= new ArrayList<Employe>();
+		employes1.add(employe1);
+		employes1.add(employe2);
+		
+		assertEquals(employes1, repoEmployeXml.obtEmployes());
+		
+		repoEmployeXml.supprimer("nomUtilisateurTest2");
+		
+		ArrayList<Employe> employes2= new ArrayList<Employe>();
+		employes2.add(employe1);
+		
+		assertEquals(employes2, repoEmployeXml.obtEmployes());
+
+	}
+
+	@Test
+	public void testModifier() {
+		repoEmployeXml.ajouter(employe1);
+		
+		Employe employe3 = Mockito.mock(Employe.class);
+		Mockito.when(employe3.obtNomUtilisateur()).thenReturn("nomUtilisateurTest1");
+		Mockito.when(employe3.obtEmail()).thenReturn("emailTestModifie");
+		
+		repoEmployeXml.modifier(employe3);
+		
+		assertEquals("emailTestModifie", repoEmployeXml.obtenir("nomUtilisateurTest1").obtEmail());
+
+		
+		
+		
+		
+	}
+
+}
