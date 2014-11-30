a48c627f3703f5f7df415ccdd5f99105b5ca10bd
Alexandra <LiberT@LiberT-PC-12345>
2014-11-30 14:48:42 -0500

    Ajout des méthodes dans la classe approbation

--- src/main/java/ca/ulaval/glo4003/projet_de_session/core/domain/Approbation.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/core/domain/Approbation.java
@@ -1,8 +1,29 @@
 package ca.ulaval.glo4003.projet_de_session.core.domain;
 
 public class Approbation {
-	private Approuvable approbation; 
-	
-	private enum etat {APPROUVEE, ATTENTE, REFUSEE}
-		
+
+	public Approbation() {
+		EtatApprobation = etat.ATTENTE;
+	}
+
+	private Approuvable approbation;
+
+	private enum etat {
+		APPROUVEE, ATTENTE, REFUSEE
+	}
+
+	private etat EtatApprobation;
+
+	private void approuver() {
+
+		EtatApprobation = etat.APPROUVEE;
+
+	}
+
+	private void Refuser() {
+
+		EtatApprobation = etat.REFUSEE;
+
+	}
+
 }
