c7a42858e5c93fbc5320da6f4dd91b6190a9d665
Alexandra <LiberT@LiberT-PC-12345>
2014-11-30 15:00:07 -0500

    Modification des constructeurs des classes feuille de temps et depenses

--- src/main/java/ca/ulaval/glo4003/projet_de_session/core/domain/Depense.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/core/domain/Depense.java
@@ -13,17 +13,18 @@ public abstract class Depense implements Approuvable {
 	private String time;
 
 	private String description;
+	
+	private Approbation approbation;
 
 	public Depense(String _identifiant, String _date, String _time, String _description ) {
 		identifiant = _identifiant;
 		date = _date;
 		time = _time;
 		description = _description;
-		
-		
+		approbation = new Approbation();
 	}
 	
-	public Depense(){}
+	public Depense(){approbation = new Approbation();}
 
 	public String obtIdentifiant() {
 		return identifiant;
