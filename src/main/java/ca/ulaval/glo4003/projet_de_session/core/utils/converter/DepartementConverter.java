6c801926e699f79e2993c401a47c5be505ebe259
mounir01 <mounir.mechqrane@gmail.com>
2014-12-01 23:47:45 -0500

    Creation RepoDepartement Modif DepartementConverter

--- src/main/java/ca/ulaval/glo4003/projet_de_session/core/utils/converter/DepartementConverter.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/core/utils/converter/DepartementConverter.java
@@ -3,18 +3,24 @@ package ca.ulaval.glo4003.projet_de_session.core.utils.converter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import ca.ulaval.glo4003.projet_de_session.core.domain.Departement;
+import ca.ulaval.glo4003.projet_de_session.core.domain.Employe;
+import ca.ulaval.glo4003.projet_de_session.core.domain.FeuilleDeTemps;
+import ca.ulaval.glo4003.projet_de_session.core.domain.Utilisateur;
 import ca.ulaval.glo4003.projet_de_session.web.viewmodels.DepartementViewModel;
 import ca.ulaval.glo4003.projet_de_session.web.viewmodels.EmployeeViewModel;
+import ca.ulaval.glo4003.projet_de_session.core.utils.FactoryDepartement;
+import ca.ulaval.glo4003.projet_de_session.core.utils.FactoryFeuilleDeTemps;
 import ca.ulaval.glo4003.projet_de_session.core.utils.converter.EmployeeConverter;
 public class DepartementConverter {
 		
-	public Collection<DepartementViewModel> convertDeps(List<Departement> entries) {
+	public Collection<DepartementViewModel> convertDeps(Map<String, Departement> entries) {
 		Collection<DepartementViewModel> viewModels = new LinkedList<DepartementViewModel>();
-		for (Departement entry : entries) {
-			DepartementViewModel viewModel = convertDep((Departement) entry);
+		for (Entry<String, Departement> entry : entries.entrySet()) {
+			DepartementViewModel viewModel = convertDep(entry.getValue());
 			viewModels.add(viewModel);
 		}
 		return viewModels;
@@ -26,8 +32,6 @@ public class DepartementConverter {
 		viewModel.setNomEntreprise(entry.obtEntreprise());
 		viewModel.setProjets(entry.obtProjets());
 		viewModel.setEmployes((ArrayList<EmployeeViewModel>) EmployeeConverter.convert(entry.obtEmployes()));
-
-		
 		return viewModel;
 	}
 	
--- src/main/java/ca/ulaval/glo4003/projet_de_session/persistence/repositoryXml/RepoDepartementXml.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/persistence/repositoryXml/RepoDepartementXml.java
@@ -1,26 +1,40 @@
 package ca.ulaval.glo4003.projet_de_session.persistence.repositoryXml;
 
 import ca.ulaval.glo4003.projet_de_session.core.domain.Departement;
+import ca.ulaval.glo4003.projet_de_session.exception.DepartementIntrouvableException;
 
 public class RepoDepartementXml extends RepoGeneralXml<Departement> {
 
-	public RepoDepartementXml(Class<Departement> type, String _nomFichierXml) {
-		super(type, _nomFichierXml);
-		// TODO Auto-generated constructor stub
+	public RepoDepartementXml() {
+		super(Departement.class, "departement");
 	}
 
 	@Override
-	public String ajouter(Departement objetAAjouter) {
-		// TODO Auto-generated method stub
-		return null;
+	public String ajouter(Departement departement) {
+		String id = construireIdentifiant(departement);
+		put(id, departement);
+			
+		return id;
 	}
-
-	@Override
-	public void modifier(Departement objetAModifier) {
-		// TODO Auto-generated method stub
+	
+	public Departement obt(String identifiantUnique){
+		Departement departement = super.obt(identifiantUnique);
 		
+		if (departement == null)
+			throw new DepartementIntrouvableException();
+		
+		return departement; 
 	}
 
+	@Override
+	public void modifier(Departement departement) {
+		modifier(construireIdentifiant(departement),departement);
+	}
+
+	
+	private String construireIdentifiant(Departement departement){
+		return departement.obtNomDepartement() + departement.obtEntreprise() ;
+		}
 	
 
 }
